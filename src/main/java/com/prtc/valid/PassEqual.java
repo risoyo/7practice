eebc677ba49a67cd8e5974e8dad7b5ac789b7229
risoyo <yinglongnvba@gmail.com>
2021-03-19 00:02:18 +0800

    初次提交，修复依赖，修复springboot2.3.1开始的@Valid注解不生效问题

---
+++ src/main/java/com/prtc/valid/PassEqual.java
@@ -0,0 +1,20 @@
+package com.prtc.valid;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PassValidator.class)
+// Constraint是指定该注解所关联的校验类是哪个
+public @interface PassEqual {
+    int min();
+    int max();
+    String message() default "";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/prtc/valid/PassValidator.java
@@ -0,0 +1,31 @@
+package com.prtc.valid;
+
+import com.prtc.dto.PersonDTO;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PassValidator implements ConstraintValidator<PassEqual, PersonDTO> {
+    private int min;
+    private int max;
+    @Override
+    public void initialize(PassEqual constraintAnnotation) {
+        this.min = constraintAnnotation.min();
+        this.max = constraintAnnotation.max();
+    }
+
+    // ConstraintValidator 传入的第一个变量是作用的注解，第二个参数是作用的对象
+    // 这里定义的是对PersonDTO生效，如果是对字段，那么这里是String
+    @Override
+    public boolean isValid(PersonDTO personDTO, ConstraintValidatorContext constraintValidatorContext){
+        String pass1 = personDTO.getPass1();
+        String pass2 = personDTO.getPass2();
+        if ((pass1.length() < min)|(pass1.length() > max)){
+            return false;
+        }
+        if ((pass2.length() < min)|(pass2.length() > max)){
+            return false;
+        }
+        return pass1.equals(pass2);
+    }
+}
